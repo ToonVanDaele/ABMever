8b83555b338a1214ed4ef95cddf80a74623b734a
VAN DAELE <toon.vandaele@inbo.be>
2022-12-12 15:58:13 +0100

    nota's overleg 12/12/2022

--- R/notas.R
+++ R/notas.R
@@ -1,18 +1,30 @@
 
 # Nog te doen:
 
+# - 1 januari is ok als startdatum?  =>  1 april start  (generiek implemeteren)
+
+# maand (kalendermaand) mee bewaren bij afschot en aantallen
+
+# afschot tweestaps relatief afschot. bv. 0.1 van totale populatie. Telkens
+# per klasse volgens vaste verhouding.
+
 # is numboar na of voor jacht in het opgegeven tijdstap?
 # volgorde: reproduce -> mortality -> hunting -> (aging)
-# bekijken of er effect is
+# bekijken of er effect is  -> waarom vershil bij scenario example.
 
 # df_harvest -> volledige populatie informatie (boar - who dies) bijhouden.
-# + sim, scenario en tijdstap.
+# + sim, scenario en tijdstap (tijd + kalendermaand) + leeftijd + geslacht
+# zowel populatie als harvest volledige populatie.
+
+
 
 # Overige nota's
 
-# - voorlopig 3 leeftijdsklassen voor populatie behouden (niet naar 4x4)
+# - voorlopig 3 leeftijdsklassen voor populatie behouden (niet naar 4 categoriën)
+# -> ? generiek implementeren
 
-# - 1 januari is ok als startdatum
+# -> figuur maken van maximale leeftijd met en zonder jacht -> maximum ongeveer 15 jaar?
+
 
 # Een afschot van een bepaald jaar -> hoe hetzelfde afschot verdelen in het jaar
 # ttz een jachtseizoen introduceren.
@@ -27,4 +39,8 @@
 # Er is geen controle over de ratios afschot t.a.v. de werkelijke populatie.
 
 # Beginnen we met de stable stage zonder jacht of stable stage met jacht?
+
+# Er is nu geen maximale leeftijd voor overleving. ? maximum instellen?
+# Of overleving leeftijdsafhankelijk? Briederman?? -> nog te bekijken (Jim)
+# repercussie ->
 
